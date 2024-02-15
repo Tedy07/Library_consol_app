b49306d83013d8959c822e949b85610823d91b1f
Ted K <ted.k07@gmail.com>
2024-02-15 10:39:07 +0100

    First commit and start applikation

--- Program.cs
+++ Program.cs
@@ -4,25 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! wprowadz swoje imię: ");
-
-            string imie = Console.ReadLine();
-            string imie2 = "Adam";
-            int liczbaCalkowita = 4;
-            bool prawdaFalsz = false;
-
-            if (string.IsNullOrEmpty(imie) == true)
-            {
-                Console.WriteLine("Nie podałeś imienia");
-            } else
-            {
-                Console.WriteLine($"Podałeś stringa: {imie}");
-            }
-
-            for (int i = 0; i < 10; i++)
-            {
-
-            }
+           
             
         }
     }
